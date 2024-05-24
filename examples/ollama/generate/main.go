9d9f29f368211aefe5e521e4abde5faaf63c9327
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-24 18:42:52 +0200

    refactor: rm comments

--- examples/ollama/generate/main.go
+++ examples/ollama/generate/main.go
@@ -7,17 +7,14 @@ import (
 ) 
 
 func main() {
-	// Define model and options
 	llama3_8b_model := ollama.OllamaModel{
 		Model:  "llama3:8b",
 		Options: ollama.ModelOptions{NumCtx: 4096},
 		Stream: false,
 	}
 
-	// Initialize a new ollamaClient instance
 	ollamaClient := ollama.NewOllamaClient(llama3_8b_model)
 
-	// Send a prompt to the model
 	prompt := "Hi, how do you do?"
 	llamaResponse, err := ollamaClient.Generate(prompt)
 	if err != nil {
