a6d5ee1eade59f52e507a3a4397a67fb07fbe177
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-25 09:22:51 +0200

    refactor: initial response

--- examples/agent-architectures/reflexion/main.go
+++ examples/agent-architectures/reflexion/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"encoding/json"
-	"log"
+	// "encoding/json"
+	// "log"
 	"os"
 
 	"github.com/TobiasGleiter/ai-agents/pkg/llms/ollama"
@@ -47,139 +47,168 @@ func main() {
 	// 4. Pass the initial prompt and response to the Revisor LLM
 	// 5. Iterate until the Revisor LLM returns true
 
-
-	// 1. User input
 	userInput := "Why is reflection useful in AI?"
 
-	// 2. Initial Response LLM.
-	// Output a JSON that can be handled accordingly.
-	// Use few shot prompting to increase the quality of the prompt.
-	initalResponseLlm := `
-	{
-		"response": "",
-		"critque": "",
-		"tools": [
-			{
-				"name": "tool_name"
-			}
-		]
-	}`
-
-	tools := []Tool{{Name: "search_internet"}, {Name: "current_weather"}, {Name: "save_file"}}
-
-	var messages []ollama.ModelMessage
-	messages = append(messages, ollama.ModelMessage{
-        Role: "system",
-        Content: fmt.Sprintf(`
-			You are a helpful AI assistant.
-			Generate an inital response along with self critque (how to improve the response) and select the right tools that helps solve the problem.
-			Tools available: %s
-			Respond in JSON format like this:
-				%s`, tools, initalResponseLlm),
-    })
-
-	messages = append(messages, ollama.ModelMessage{
-		Role: "user",
-		Content: userInput,
-	})
-
-	llamaRequest := ollama.Model{
+	llama3_8b_model := ollama.OllamaModel{
 		Model:  "llama3:8b",
-		Messages: messages,
-		Options: ollama.ModelOptions{
-			Temperature: 1,
-			NumCtx: 4096,
-		},
+		Options: ollama.ModelOptions{NumCtx: 4096},
 		Stream: false,
 		Format: "json",
 	}
 
-	var response InitialResponse
-	res, err := ollama.Chat(llamaRequest)
+	initialResponseLlm := ollama.NewOllamaClient(llama3_8b_model)
 
-	err = json.Unmarshal([]byte(res.Message.Content), &response)
-	if err != nil {
-		log.Fatalf("Failed to decode JSON: %s", err)
-	}
+	tools := []Tool{{Name: "search_internet"}, {Name: "current_weather"}, {Name: "save_file"}}
+	systemPrompt := fmt.Sprintf(`
+		You are a helpful AI assistant.
+		Generate an inital response along with self critque (how to improve the response) and select the right tools that helps solve the problem.
+		Tools available: %s
+		Respond in JSON format like this:
+		{
+			"response": "",
+			"critque": "",
+			"tools": [
+				{
+					"name": "tool_name"
+				}
+			]
+		}`, tools)
 
-	ChatColor.PrintColor(ChatColor.Yellow, "Initial Response: " + string(response.Response))
-	ChatColor.PrintColor(ChatColor.Cyan, "Initial Critque: " + string(response.Critque))
-	ChatColor.PrintColor(ChatColor.Green, "Tools: " + fmt.Sprintf("%s", response.Tools))
+	var fewShotMessages []ollama.ModelMessage
+	fewShotMessages = append(fewShotMessages, ollama.ModelMessage{
+		Role: "user",
+		Content: "When is the next US president election?", // Necessary to add "Respond in JSON" or there will be many whitespaces
+	})
+	fewShotMessages = append(fewShotMessages, ollama.ModelMessage{
+		Role: "assistant",
+		Content: `
+			"response": "I dont have the right answer, I need to search the internet.",
+			"critque": "",
+			"tools": [
+				{
+					"name": "search_internet",
+				}
+			]
+		}`,
+	})
 
-	// 3. Use the tool(s)
-	for i := 0; i < len(response.Tools); i++ {
-		if function, exists := functions[response.Tools[i].Name]; exists {
-			function()
-		} else {
-			fmt.Printf("No function found. \n")
-		}
-	}
+	initialResponseLlm.SetSystemPrompt(systemPrompt)
+	initialResponseLlm.SetMessages(fewShotMessages)
+	initialResponseLlm.Chat(userInput)
+
+	// var messages []ollama.ModelMessage
+	// messages = append(messages, ollama.ModelMessage{
+    //     Role: "system",
+    //     Content: fmt.Sprintf(`
+	// 		You are a helpful AI assistant.
+	// 		Generate an inital response along with self critque (how to improve the response) and select the right tools that helps solve the problem.
+	// 		Tools available: %s
+	// 		Respond in JSON format like this:
+	// 			%s`, tools, initalResponseLlm),
+    // })
+
+	// messages = append(messages, ollama.ModelMessage{
+	// 	Role: "user",
+	// 	Content: userInput,
+	// })
+
+	// llamaRequest := ollama.Model{
+	// 	Model:  "llama3:8b",
+	// 	Messages: messages,
+	// 	Options: ollama.ModelOptions{
+	// 		Temperature: 1,
+	// 		NumCtx: 4096,
+	// 	},
+	// 	Stream: false,
+	// 	Format: "json",
+	// }
+
+	// var response InitialResponse
+	// res, err := ollama.Chat(llamaRequest)
+
+	// err = json.Unmarshal([]byte(res.Message.Content), &response)
+	// if err != nil {
+	// 	log.Fatalf("Failed to decode JSON: %s", err)
+	// }
+
+	// ChatColor.PrintColor(ChatColor.Yellow, "Initial Response: " + string(response.Response))
+	// ChatColor.PrintColor(ChatColor.Cyan, "Initial Critque: " + string(response.Critque))
+	// ChatColor.PrintColor(ChatColor.Green, "Tools: " + fmt.Sprintf("%s", response.Tools))
+
+	// // 3. Use the tool(s)
+	// for i := 0; i < len(response.Tools); i++ {
+	// 	if function, exists := functions[response.Tools[i].Name]; exists {
+	// 		function()
+	// 	} else {
+	// 		fmt.Printf("No function found. \n")
+	// 	}
+	// }
 
 
-	var revisorMessages []ollama.ModelMessage
-	// 4. Pass the initial prompt and response to the Revisor LLM
-	revisorMessages = append(revisorMessages, ollama.ModelMessage{
-        Role: "system",
-        Content: fmt.Sprintf(`
-			You are a revisor AI assistant.
+	// var revisorMessages []ollama.ModelMessage
+	// // 4. Pass the initial prompt and response to the Revisor LLM
+	// revisorMessages = append(revisorMessages, ollama.ModelMessage{
+    //     Role: "system",
+    //     Content: fmt.Sprintf(`
+	// 		You are a revisor AI assistant.
 
-			Generate a new better response along as a new critque to improve the response.
+	// 		Generate a new better response along as a new critque to improve the response.
 			
-			Respond in JSON format like this:
-				%s`, initalResponseLlm),
-    })
+	// 		Respond in JSON format like this:
+	// 			%s`, initalResponseLlm),
+    // })
 
-	limit := 4
-	for j := 0; j < limit; j++ {
-		revisorMessages = append(revisorMessages, ollama.ModelMessage{
-			Role: "user",
-			Content: fmt.Sprintf(`
-				Generate a new better response along as a new critque to improve the response.
+	// limit := 4
+	// for j := 0; j < limit; j++ {
+	// 	revisorMessages = append(revisorMessages, ollama.ModelMessage{
+	// 		Role: "user",
+	// 		Content: fmt.Sprintf(`
+	// 			Generate a new better response along as a new critque to improve the response.
 
-				Response: %s
-				Critque: %s
+	// 			Response: %s
+	// 			Critque: %s
 
-				Respond in JSON.`, response.Response, response.Critque),
-		})
+	// 			Respond in JSON.`, response.Response, response.Critque),
+	// 	})
 
-		llamaRevisorRequest := ollama.Model{
-			Model:  "llama3:8b",
-			Messages: revisorMessages,
-			Options: ollama.ModelOptions{
-				Temperature: 0.8,
-				NumCtx: 4096,
-			},
-			Stream: false,
-			Format: "json",
-		}
+	// 	llamaRevisorRequest := ollama.Model{
+	// 		Model:  "llama3:8b",
+	// 		Messages: revisorMessages,
+	// 		Options: ollama.ModelOptions{
+	// 			Temperature: 0.8,
+	// 			NumCtx: 4096,
+	// 		},
+	// 		Stream: false,
+	// 		Format: "json",
+	// 	}
 
-		res, err = ollama.Chat(llamaRevisorRequest)
-		err = json.Unmarshal([]byte(res.Message.Content), &response)
-		if err != nil {
-			log.Fatalf("Failed to decode JSON: %s", err)
-		}
+	// 	res, err = ollama.Chat(llamaRevisorRequest)
+	// 	err = json.Unmarshal([]byte(res.Message.Content), &response)
+	// 	if err != nil {
+	// 		log.Fatalf("Failed to decode JSON: %s", err)
+	// 	}
 
-		jsonResponse, err := json.Marshal(response)
-		if err != nil {
-			log.Fatalf("Failed to encode JSON: %s", err)
-		}
+	// 	jsonResponse, err := json.Marshal(response)
+	// 	if err != nil {
+	// 		log.Fatalf("Failed to encode JSON: %s", err)
+	// 	}
 
-		revisorMessages = append(revisorMessages, ollama.ModelMessage{
-			Role: "assistant",
-			Content: string(jsonResponse),
-		})
+	// 	revisorMessages = append(revisorMessages, ollama.ModelMessage{
+	// 		Role: "assistant",
+	// 		Content: string(jsonResponse),
+	// 	})
 
-		ChatColor.PrintColor(ChatColor.Yellow, "Initial Response: " + string(response.Response))
-		ChatColor.PrintColor(ChatColor.Cyan, "Initial Critque: " + string(response.Critque))
-		ChatColor.PrintColor(ChatColor.Yellow, fmt.Sprintf("Revision: %v", j))
-	}
+	// 	ChatColor.PrintColor(ChatColor.Yellow, "Initial Response: " + string(response.Response))
+	// 	ChatColor.PrintColor(ChatColor.Cyan, "Initial Critque: " + string(response.Critque))
+	// 	ChatColor.PrintColor(ChatColor.Yellow, fmt.Sprintf("Revision: %v", j))
+	// }
 
-	ChatColor.PrintColor(ChatColor.Green, "Final Response: " + response.Response)
+	// ChatColor.PrintColor(ChatColor.Green, "Final Response: " + response.Response)
 
-	err = writeMessagesToMarkdown(revisorMessages, "messages.md")
-    if err != nil {
-        log.Fatalf("Error writing to markdown file: %s", err)
-    }
+	// err = writeMessagesToMarkdown(revisorMessages, "messages.md")
+    // if err != nil {
+    //     log.Fatalf("Error writing to markdown file: %s", err)
+    // }
 }
 
 
